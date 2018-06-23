2a081699ac7a7bb94543dc15eef6a429794622c4
b3n10 <francisben.lleve@gmail.com>
2018-06-24 01:25:04 +0800

    Update index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
+// main entry point of app
 
-echo 'Requested URL: ' . $_SERVER['QUERY_STRING'];
+require '../Core/Router.php';
+
+$router = new Router();
+
+$router->add('', [
+	'controller'	=>	'Home',
+	'action'			=>	'index'
+]);
+$router->add('posts', [
+	'controller'	=>	'Posts',
+	'action'			=>	'index'
+]);
+$router->add('posts/new', [
+	'controller'	=>	'Posts',
+	'action'			=>	'new'
+]);
+
+$url = $_SERVER['QUERY_STRING'];
+
+$router->match($url);
+var_dump($router->getParams());
