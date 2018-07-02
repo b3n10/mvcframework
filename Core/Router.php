d0f5dded0b916cf3e5128abd91c947837787717a
b3n10 <francisben.lleve@gmail.com>
2018-07-02 20:19:02 +0800

    Revised add to replace routes with regex, match to check if url matches route

--- Core/Router.php
+++ Core/Router.php
@@ -23,8 +23,17 @@ class Router
 	 *
 	 * @return void
 	 */
-	public function add($route, $params)
+	public function add($route, $params = [])
 	{
+		// Escape forward slashes
+		$route = preg_replace('/\//', '\\/', $route);
+
+		// Convert variables (ex: controller)
+		$route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+		// Add start, end delimiters, case insensitive flag
+		$route = '/^' . $route . '$/i';
+
 		$this->routes[$route] = $params;
 	}
 
@@ -47,21 +56,16 @@ class Router
 	 */
 	public function match($url)
 	{
-		$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-
-		if (preg_match($reg_exp, $url, $matches))
-		{
-			$params = [];
-
-			foreach ($matches as $key => $value)
-			{
-				if (is_string($key))
-				{
-					$params[$key] = $value;
+		foreach ($this->routes as $route => $params) {
+			if (preg_match($route, $url, $matches)) {
+				foreach ($matches as $key => $value) {
+					if (is_string($key)) {
+						$params[$key] = $value;
+					}
 				}
+				$this->params = $params;
+				return true;
 			}
-			$this->params = $params;
-			return true;
 		}
 
 		return false;
