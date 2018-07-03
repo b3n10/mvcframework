f1aaea5f68d1398d6dff7ed3185849312f1e360d
b3n10 <francisben.lleve@gmail.com>
2018-07-03 17:52:02 +0800

    Add optional namespace

--- public/index.php
+++ public/index.php
@@ -18,13 +18,11 @@ $router->add('', [
 	'controller'	=>	'Home',
 	'action'			=>	'index'
 ]);
-$router->add('posts', [
-	'controller'	=>	'Posts',
-	'action'			=>	'index'
-]);
 $router->add('{controller}/{action}');
-$router->add('admin/{action}/{controller}');
 $router->add('{controller}/{id:\d+}/{action}');
+$router->add('admin/{controller}/{action}', [
+	'namespace'	=>	'Admin'
+]);
 
 $url = $_SERVER['QUERY_STRING'];
 
