aa10874938aaa3e85f20fe48b012873e36c3ad22
b3n10 <francisben.lleve@gmail.com>
2018-07-03 18:11:37 +0800

    Add index method calling render method

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -1,2 +1,15 @@
 <?php
 
+namespace App\Controllers;
+
+use \Core\View;
+
+class Home extends \Core\Controller {
+	protected function before() {
+		return true;
+	}
+
+	public function indexAction() {
+		View::render('Home/index.php');
+	}
+}
