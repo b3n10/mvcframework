493bc5ead02768c00eb6c061e591fd84fd01fa45
b3n10 <francisben.lleve@gmail.com>
2018-07-03 15:54:40 +0800

    Add call magic method with before, after

--- Core/Controller.php
+++ Core/Controller.php
@@ -10,4 +10,31 @@ abstract class Controller
 	{
 		$this->route_params = $params;
 	}
+
+	public function __call($name, $args)
+	{
+		$method = $name . 'Action';
+
+		if (method_exists($this, $method))
+		{
+			if ($this->before())
+			{
+				call_user_func_array([$this, $method], $args);
+				$this->after();
+			}
+		}
+		else
+		{
+			echo "Method '$name' doesn't exists in '" . get_class($this) . "'";
+		}
+	}
+
+	protected function before()
+	{
+	}
+
+	protected function after()
+	{
+	}
+
 }
